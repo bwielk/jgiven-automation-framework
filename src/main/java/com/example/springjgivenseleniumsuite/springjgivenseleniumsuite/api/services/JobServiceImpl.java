78ebfec43efe374aeda1fe11f25b0ef0d2db27b5
Blaise Wielk <bwielk@gmail.com>
2021-06-14 16:51:44 +0100

    Introduced some Slf4j logging in some classes

--- src/main/java/com/example/springjgivenseleniumsuite/springjgivenseleniumsuite/api/services/JobServiceImpl.java
+++ src/main/java/com/example/springjgivenseleniumsuite/springjgivenseleniumsuite/api/services/JobServiceImpl.java
@@ -4,12 +4,14 @@ import com.example.springjgivenseleniumsuite.springjgivenseleniumsuite.api.clien
 import com.example.springjgivenseleniumsuite.springjgivenseleniumsuite.api.model.AssociatedSkillsDTO;
 import com.example.springjgivenseleniumsuite.springjgivenseleniumsuite.api.model.JobDTO;
 import com.example.springjgivenseleniumsuite.springjgivenseleniumsuite.api.model.SkillsDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 public class JobServiceImpl {
 
@@ -19,14 +21,18 @@ public class JobServiceImpl {
 
 
     public List<JobDTO> getJobsAssociatedWithWord(String word){
+        log.info(String.format("getJobsAssociatedWithWord - getting job associated with a word '%s'", word));
         return jobApi.getJobsByAssociatedWork(word);
     }
 
     public SkillsDTO getSkillsAssociatedWithJob(String jobUUID){
+        log.info(String.format("getSkillsAssociatedWithJob - getting skills associated with a job of UUID '%s'", jobUUID));
         return jobApi.getSkillsPerJobUUID(jobUUID);
     }
 
     public AssociatedSkillsDTO getSkillsAssociatedWithASkillByUUID(String skillUUID){
+        log.info(String.format("getSkillsAssociatedWithASkillByUUID - getting skills associated with a skill of" +
+                " UUID '%s'", skillUUID));
         return jobApi.getAssociatedSkillsBySkillUUID(skillUUID);
     }
 }
